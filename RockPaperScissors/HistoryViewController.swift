f60c7c14c2a82eb9ebea528ccd8142ca79db83cf
Christian Villere <Christianvillere@gmail.com>
2017-10-01 15:47:28 -0700

    Initial Commit

---
+++ RockPaperScissors/HistoryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HistoryViewController.swift
+//  RockPaperScissors
+//
+//  Created by Christian Villere on 10/1/17.
+//  Copyright © 2017 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HistoryViewController: UIViewController {
+    
+    
+    //MARK: Properties
+    
+    var history: [RPSMatch]!
+    
+    
+    override func viewDidLoad(){
+        
+        
+        
+    
+    }
+    
+    
+    
+    
+    
+    
+
+    
+    
+}
