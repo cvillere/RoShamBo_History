cc2167d1cd1ab5b6bdc676b802407c1d4ca36cf4
Christian Villere <Christianvillere@gmail.com>
2017-10-08 22:17:01 -0700

    Add TableView to HistoryViewController

--- RockPaperScissors/HistoryViewController.swift
+++ RockPaperScissors/HistoryViewController.swift
@@ -9,20 +9,37 @@
 import Foundation
 import UIKit
 
-class HistoryViewController: UIViewController {
+class HistoryViewController: UIViewController, UITableViewDataSource {
+    
+    //Mark: Outlets
+    
+    @IBOutlet weak var tableHistory: UITableView!
     
     
     //MARK: Properties
     
-    var history: [RPSMatch]!
+    var history = [RPSMatch]()
     
     
-    override func viewDidLoad(){
+    //MARK: Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
+        return history.count
         
-        
-    
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ROC_history", for: indexPath)
+        let match = self.history[indexPath.row]
+        //struggling with how to implement below
+        //cell.textLabel?.text = "\(winner) beats \(loser)"
+        return cell
+        
+    }
+    
+    
+
     
     
     
